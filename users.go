2a33c86a945e127e8dfb0e0fb3f0130f2d44e38f
CharlieJ2005 <163343248+CharlieJ2005@users.noreply.github.com>
2025-08-19 23:41:38 +0200

    fix: struct json tags

--- users.go
+++ users.go
@@ -4,17 +4,23 @@ import (
 	"encoding/json"
 	"net/http"
 	"time"
+
+	"github.com/google/uuid"
 )
 
-func (cfg *apiConfig) handlerUsers(w http.ResponseWriter, r *http.Request) {
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Email     string    `json:"email"`
+}
+
+func (cfg *apiConfig) handlerUsersCreate(w http.ResponseWriter, r *http.Request) {
 	type parameters struct {
 		Email string `json:"email"`
 	}
-	type returnVals struct {
-		ID        string    `json:"id"`
-		CreatedAt time.Time `json:"created_at"`
-		UpdatedAt time.Time `json:"updated_at"`
-		Email     string    `json:"email"`
+	type response struct {
+		User
 	}
 
 	decoder := json.NewDecoder(r.Body)
@@ -31,10 +37,12 @@ func (cfg *apiConfig) handlerUsers(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	respondWithJSON(w, http.StatusCreated, returnVals{
-		ID:        user.ID.String(),
-		CreatedAt: user.CreatedAt.UTC(),
-		UpdatedAt: user.UpdatedAt.UTC(),
-		Email:     user.Email,
+	respondWithJSON(w, http.StatusCreated, response{
+		User: User{
+			ID:        user.ID,
+			CreatedAt: user.CreatedAt,
+			UpdatedAt: user.UpdatedAt,
+			Email:     user.Email,
+		},
 	})
 }
