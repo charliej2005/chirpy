d494aa2dbd754c7416064376a9d5049fa6787294
CharlieJ2005 <163343248+CharlieJ2005@users.noreply.github.com>
2025-08-21 19:56:17 +0200

    fix: delete users

--- handler_reset.go
+++ handler_reset.go
@@ -9,7 +9,11 @@ func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
 	}
 
 	cfg.fileserverHits.Store(0)
-	cfg.db.DeleteAllUsers(r.Context())
+	err := cfg.db.DeleteAllUsers(r.Context())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Failed to delete users", err)
+		return
+	}
 
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Reset data"))
